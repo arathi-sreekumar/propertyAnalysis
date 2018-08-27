1fb5ee19c4c78e307ebfba620c2be066c9c1e9a1
Arathi Sreekumar <arathi@Arathis-Air.home>
2018-08-27 22:50:37 +0100

    Fixed flow issue

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -4,12 +4,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 
-// declare var module: {
-// 	hot: {
-// 		accept(path: string, callback: () => void): void
-// 	}
-// };
-
 const renderApp = () => {
 	render(<App />, document.getElementById('app'));
 };
